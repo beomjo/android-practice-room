be535ec83401cdaa5e10c4aad1405520701a2c31
beomjo <qjatjr1108@gmail.com>
2021-01-27 17:22:42 +0900

    feat: Import CodeLab Starting Code

---
+++ advanced-coroutine-codelab/app/src/main/java/com/beomjo/coroutines/advanced/PlantListViewModel.kt
@@ -0,0 +1,70 @@
+package com.beomjo.coroutines.advanced
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import com.beomjo.advancedcoroutines.GrowZone
+import com.beomjo.advancedcoroutines.NoGrowZone
+import com.beomjo.advancedcoroutines.Plant
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class PlantListViewModel internal constructor(
+    private val plantRepository: PlantRepository
+) : ViewModel() {
+
+    private val _snackbar = MutableLiveData<String?>()
+    val snackbar: LiveData<String?>
+        get() = _snackbar
+
+    private val _spinner = MutableLiveData<Boolean>(false)
+    val spinner: LiveData<Boolean>
+        get() = _spinner
+
+    private val growZone = MutableLiveData<GrowZone>(NoGrowZone)
+
+    val plants: LiveData<List<Plant>> = growZone.switchMap { growZone ->
+        if (growZone == NoGrowZone) {
+            plantRepository.plants
+        } else {
+            plantRepository.getPlantsWithGrowZone(growZone)
+        }
+    }
+
+    init {
+        clearGrowZoneNumber()
+    }
+
+    fun setGrowZoneNumber(num: Int) {
+        growZone.value = GrowZone(num)
+
+        launchDataLoad { plantRepository.tryUpdateRecentPlantsCache() }
+    }
+
+    fun clearGrowZoneNumber() {
+        growZone.value = NoGrowZone
+
+        launchDataLoad { plantRepository.tryUpdateRecentPlantsCache() }
+    }
+
+    fun isFiltered() = growZone.value != NoGrowZone
+
+    fun onSnackbarShown() {
+        _snackbar.value = null
+    }
+
+    private fun launchDataLoad(block: suspend () -> Unit): Job {
+        return viewModelScope.launch {
+            try {
+                _spinner.value = true
+                block()
+            } catch (error: Throwable) {
+                _snackbar.value = error.message
+            } finally {
+                _spinner.value = false
+            }
+        }
+    }
+}
--- advanced-coroutine-codelab/app/src/main/java/com/beomjo/coroutines/advanced/ui/MainActivity.kt
+++ advanced-coroutine-codelab/app/src/main/java/com/beomjo/coroutines/advanced/ui/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.beomjo.coroutines.advanced
+package com.beomjo.coroutines.advanced.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.beomjo.coroutines.advanced.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
