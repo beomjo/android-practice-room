56cb5bee67ad96fa1b5a159e34e9a9151e4d3590
beomjo <qjatjr1108@gmail.com>
2021-02-10 00:33:18 +0900

    feat: @Module, @Binds, @BindsInstance 어노테이션 추가

--- dagger2-codelab/app/src/main/java/com/beomjo/dagger/di/AppComponent.kt
+++ dagger2-codelab/app/src/main/java/com/beomjo/dagger/di/AppComponent.kt
@@ -1,9 +1,18 @@
 package com.beomjo.dagger.di
 
+import android.content.Context
 import com.beomjo.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
 import dagger.Component
 
-@Component
+@Component(modules = [StorageModule::class])
 interface AppComponent {
+
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
     fun inject(activity: RegistrationActivity)
 }
