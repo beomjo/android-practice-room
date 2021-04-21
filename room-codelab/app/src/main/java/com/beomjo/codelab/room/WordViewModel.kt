9143ef781f7afdc542f29877db5610cb0fe12edb
beomjo <qjatjr1108@gmail.com>
2021-04-22 01:25:19 +0900

    feat: Create WordViewModel

---
+++ room-codelab/app/src/main/java/com/beomjo/codelab/room/WordViewModel.kt
@@ -0,0 +1,25 @@
+package com.beomjo.codelab.room
+
+import androidx.lifecycle.*
+import com.beomjo.codelab.room.persistence.entity.Word
+import com.beomjo.codelab.room.repository.WordRepository
+import kotlinx.coroutines.launch
+import java.lang.IllegalStateException
+
+class WordViewModel(private val repository: WordRepository) : ViewModel() {
+    val allWords: LiveData<List<Word>> = repository.allWords.asLiveData()
+
+    fun insert(word: Word) = viewModelScope.launch {
+        repository.insert(word)
+    }
+}
+
+class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return WordViewModel(repository) as T
+        }
+        throw IllegalStateException("Unknown ViewModel Class")
+    }
+}
