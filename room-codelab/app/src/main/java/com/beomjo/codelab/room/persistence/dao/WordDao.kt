f2ee2eb9fab4861ade2fa07b341214082192eb22
beomjo <qjatjr1108@gmail.com>
2021-04-22 01:03:53 +0900

    feat: Create Word Dao

---
+++ room-codelab/app/src/main/java/com/beomjo/codelab/room/persistence/dao/WordDao.kt
@@ -0,0 +1,21 @@
+package com.beomjo.codelab.room.persistence.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.beomjo.codelab.room.persistence.entity.Word
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizedWord(): Flow<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
